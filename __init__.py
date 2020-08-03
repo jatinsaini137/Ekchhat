53a157fa393904ceb472d8ccbf4a81c42a96d78e
jatinsaini137 <jatinrajbagri2001@gmail.com>
2020-08-03 22:06:13 +0530

    sqlite3 error

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ad66d90a56b675248acb31307cca3cc8017fc2ac'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:/Users/bittu/Desktop/EkChhat/EkChhat/test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
 app.config['SQLAlCHEMY_TRACK_MODIFICATION'] = True
 db = SQLAlchemy(app)
 
